a75e01fe9871c455354d7c6f54085cf74e2ffd29
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-03-13 15:01:19 +0100

    Create top function

---
+++ R/top.R
@@ -0,0 +1,17 @@
+#' Percentage of top species
+#'
+#' @param graph an igraph object.
+#'
+#' @return numeric: percentage of top species.
+#' @export
+#'
+#' @examples
+#' @references
+top <- function(graph){
+
+  stopifnot(class(graph) == "igraph")
+
+  res <- igraph::degree(graph, mode = "in") # in == node only giving interactions
+
+  return(length(res[res == 0])/igraph::gorder(graph)*100)
+}
