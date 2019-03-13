14d29a5a99ef81cfa79d1c419f46486acdd03038
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-03-13 12:33:08 +0100

    Create clustering coefficient function

---
+++ R/clustering_coefficient.R
@@ -0,0 +1,17 @@
+#' Cluestering coefficient
+#'
+#' @param graph an igraph object.
+#' @param type caracter: the type of the transitivity to calculate. See \code{?igraph::transitivity} for more informations.
+#' @param ... others arguments passed to \code{igraph::transitivity}.
+#'
+#' @return numeric: the clustering coefficient.
+#' @export
+#'
+#' @examples
+#' @references Baiser, B., Gotelli, N. J., Buckley, H. L., Miller, T. E., & Ellison, A. M. (2012). Geographic variation in network structure of a nearctic aquatic food web. Global Ecology and Biogeography.
+cl <- function(graph, type = "global", ...){
+
+  stopifnot(class(graph) == "igraph")
+
+  return(igraph::transitivity(graph, type = type, eval(substitute(alist(...)))))
+}
